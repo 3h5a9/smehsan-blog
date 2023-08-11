d375b63ff0e2f7945fcc6df92736c6ec75d09ab4
digiappdev <syedmdehsan@yahoo.com>
2023-08-11 21:33:23 +0100

    bug fix on home page

--- app/Main.tsx
+++ app/Main.tsx
@@ -3,27 +3,33 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 import NewsletterForm from 'pliny/ui/NewsletterForm'
-import Image from 'next/image'
-
 
 const MAX_DISPLAY = 5
+
+
 
 const links = [
   { name: 'Ruby on Rail', href: '#' },
   { name: 'WordPress', href: '#' },
 ]
+const stats = [
+  { name: 'Offices worldwide', value: '12' },
+  { name: 'Full-time colleagues', value: '300+' },
+  { name: 'Hours per week', value: '40' },
+  { name: 'Paid time off', value: 'Unlimited' },
+]
+
+
 
 export default function Home({ posts }) {
   return (
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="relative isolate overflow-hidden bg-gray-900 py-2 sm:py-12">
-          <Image
-            src={'/static/images/ruby_on_rails.webp'}
-            className='absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center'
-            alt='Ruby on Rail - Image Credit Shakuro.com'
-            width="1024"
-            height="312"
+          <img
+            src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+            alt=""
+            className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
           />
           <div
             className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
@@ -51,9 +57,9 @@ export default function Home({ posts }) {
           </div>
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="mx-auto max-w-2xl lg:mx-0">
-              <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">Syed Mohmmed Ehsan</h2>
+              <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">SM Ehsan</h2>
               <p className="mt-6 text-lg leading-8 text-gray-300">
-                Ruby on Rails And WordPress Developer. I am from Bangladesh and Currently living in Londo.
+                Ruby on Rails && WordPress Developer. Currently living in London.
               </p>
             </div>
             <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
@@ -64,13 +70,30 @@ export default function Home({ posts }) {
                   </a>
                 ))}
               </div>
+              <dl className="mt-8 grid grid-cols-1 gap-8 sm:mt-10 sm:grid-cols-2 lg:grid-cols-4">
+                {stats.map((stat) => (
+                  <div key={stat.name} className="flex flex-col-reverse">
+                    <dt className="text-base leading-7 text-gray-300">{stat.name}</dt>
+                    <dd className="text-2xl font-bold leading-9 tracking-tight text-white">{stat.value}</dd>
+                  </div>
+                ))}
+              </dl>
             </div>
           </div>
         </div>
 
+
+
+
+
+
+
+
+
+
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-          <h1 className="text-xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Latest from Blog
+          <h1 className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Latest
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
